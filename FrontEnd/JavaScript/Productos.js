95b367d5fa3b844e798336953c4d7094eec5cbd3
JoseZenteno269 <zentenojose123456@gmail.com>
2026-08-27 23:25:40 -0300

    feat and fix "swagger., productos.html y productos.js"

--- FrontEnd/JavaScript/Productos.js
+++ FrontEnd/JavaScript/Productos.js
@@ -1,2 +1,35 @@
-const host = "https://localhost:7040/api"
+const host = "https://localhost:7040/api/Productos"
 
+import { crearCelda } from "./funciones"
+
+async function cargarProductos() {
+    const respuesta_prod = await fetch(`${host}/${"productos"}`); 
+    const productos = await respuesta_prod.json(); 
+    const tablacuerpo = document.getElementById("cuerpo_tabla"); 
+
+    tablacuerpo.replaceChild(); 
+
+    productos.array.forEach(prod => {
+        const tr = document.createElement("tr"); 
+
+        let codigo = crearCelda(prod.codigo); 
+        let nombre = crearCelda(prod.nombre); 
+        let descripcion = crearCelda(prod.descripcion); 
+        let precio = crearCelda(prod.precio); 
+        let stock = crearCelda(prod.stock); 
+        let imagen = crearCelda(prod.imagen); 
+
+        tr.appendChild(codigo); 
+        tr.appendChild(nombre); 
+        tr.appendChild(descripcion); 
+        tr.appendChild(precio); 
+        tr.appendChild(stock); 
+        tr.appendChild(imagen); 
+
+        tablacuerpo.appendChild(tr); 
+    });
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    await cargarProductos(); 
+});
---
+++ FrontEnd/JavaScript/funciones.js
@@ -0,0 +1,5 @@
+export function crearCelda(texto){
+    const td = document.createElement("td");
+    td.textContent = texto;
+    return td; 
+}
