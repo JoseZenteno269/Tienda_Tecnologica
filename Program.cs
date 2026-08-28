610466554e06f5813f5750cecd81fed6afb04e2b
JoseZenteno269 <zentenojose123456@gmail.com>
2026-08-28 16:55:02 -0300

    fix: Algunos arreglos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,70 @@
+// // var builder = WebApplication.CreateBuilder(args);
+
+// // // Add services to the container.
+// // // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// // builder.Services.AddOpenApi();
+
+// // var app = builder.Build();
+
+// // // Configure the HTTP request pipeline.
+// // if (app.Environment.IsDevelopment())
+// // {
+// //     app.MapOpenApi();
+// // }
+
+// // app.UseHttpsRedirection();
+
+// // var summaries = new[]
+// // {
+// //     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// // };
+
+// // app.MapGet("/weatherforecast", () =>
+// // {
+// //     var forecast =  Enumerable.Range(1, 5).Select(index =>
+// //         new WeatherForecast
+// //         (
+// //             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+// //             Random.Shared.Next(-20, 55),
+// //             summaries[Random.Shared.Next(summaries.Length)]
+// //         ))
+// //         .ToArray();
+// //     return forecast;
+// // })
+// // .WithName("GetWeatherForecast");
+
+// // app.Run();
+
+// // record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// // {
+// //     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// // }
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Controllers
+// builder.Services.AddControllers();
+
+// // Swagger
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+// var app = builder.Build();
+
+// // Swagger
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+// app.MapControllers();
+
+// app.Run();
+
 // var builder = WebApplication.CreateBuilder(args);
 
 // // Add services to the container.
@@ -48,6 +115,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// CORS
+builder.Services.AddCors(opciones =>
+{
+    opciones.AddPolicy("PermitirTodo", politica =>
+    {
+        politica.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Swagger
@@ -58,6 +136,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("PermitirTodo");
 
 app.UseAuthorization();
 
